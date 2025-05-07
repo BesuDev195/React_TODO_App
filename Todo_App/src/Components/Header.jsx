4a5993526d5405cd50dc10a6b3df9fb7273699e4
BesuDev195 <besobelagit1921@gmail.com>
2025-05-07 23:13:51 +0300

    middler

---
+++ Todo_App/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./Header.module.css"
+
+export default function Header() {
+  return (
+    <div>
+        <h2 className={styles.Header}>TODO list</h2>
+    </div>
+  )
+}
---
+++ Todo_App/src/Components/Todo.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Tasks from "./Tasks";
+import styles from "./Todo.module.css"
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTasks([...tasks, todo]);
+    setTodo("");
+  }
+  return (
+    <div className={styles.form}>
+      <form className={styles.wow_InputContainer} onSubmit={handleSubmit}>
+        <input
+          className={styles.wow_Input}
+          placeholder="Enter Your Task Here"
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button className={styles.butt}>Add Task</button>
+      </form>
+      <div className={styles.List}>
+        {tasks.map((task, index) => (
+          <Tasks key={index} task={task}/>
+          
+            
+        ))}
+        
+      </div>
+    </div>
+  );
+}
